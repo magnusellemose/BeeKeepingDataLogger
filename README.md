534daaa5f581d49fdefd3652d6e92d95b5c6b93b
magnusellemose <70758683+magnusellemose@users.noreply.github.com>
2020-10-27 15:41:40 +0100

    Development (#2)
    * Add files via upload
    
    Added functionality so that the transmitter and the receiver uses the RTC chip to figure out when to send the data.
    Also added a datalogging function on the receiver
    
    * Delete ReceiverWithRTCAndDatalogging.ino
    
    Should just be named Receiver
    
    * Delete TransmitterWithRTC.ino
    
    Should be named Transmitter
    
    * Replacement og transmitter and receiver code
    
    Added RTC functionality for the Transmitter, so that it transmits data at a given time interval.
    Added datalogging functionality for the Receiver, so that the received data is logged in an Excel-file
    
    * Code cleanup and refactoring
    
    No new functionality added
    
    * Update README.md
    
    Added actual text in the readme-file

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # BeeKeepingDataLogger
-An Arduino project where data is read from beehives and sent to and saved in a central computer
+The purpose of the project is to help a local beekeeper and to gain experience with working with new technologies and programming languages in a project. The purpose is NOT to make a profit.
+
+The goal of the project is to create a system that can measure the temperature and humidity of a set of beehives and log the data in a centralized computer, so the owner can see exactly when his bees start to become active again during the spring. When the bees start to become active again, they raise the temperature of the beehive from around 24 degrees celsius to above 30 degrees celsius. The humidity of the beehives is useful because if the humidity falls below 50%, no eggs are hatched. A healthy colony has a 
+
+To accomplish this goal, Arduino hardware components and Arduino software are used to create transmitters and receivers (and intermediate transceiver later on). The transmitter exploits Arduino hardware components to measure the temperature and humidity of a beehive. The receiver exploits Arduino hardware components to receive data from transmitters and to log it in an Excel file.
+
+There are two people working on the project: one who does the software(me) and one who does the hardware(my flatmate).
+
+
+//======================================================================//
+The initial plan of the project is to assist a local beekepper keeping track of the temperature and humidity of his currently 7 beehives. The amount of beehives is excepted to rise but not above 12 in the nearest future.
+
+If the project becomes a succes, another local beekeeper with more than 30 beehives would be willing to acquire the system.
+
+
+//======================================================================//
+The main programs for this project is:
+Arduino IDE:
+- To develop the code.
+
+Fritzing:
+- To create sketches for the configuation of the hardware.
+
+PLX-DAQ v2.11:
+- To log the received data in an Excel spreadsheet.
+
+Microsoft Excel
+- To log received data and to communicate the results to an end user.
+
+On some computers, it is necessary to install the CH340g driver to get unoriginal Arduino boards to work. It can be downloaded following this link: 
+- 32 bit: https://ardustore.dk/error/CH341SER_32_bit.zip 
+- 64 bit: https://ardustore.dk/error/CH341SER_64_bit.zip
+
+
+//======================================================================//
+More technical description of the components:
+Transmitter:
+- Hardware components:
+    - Arduino board
+    - DHT chip (to measure temperature and humidity)
+    - NRF24l01+ chip (to transmit data)
+    - DS3231 I2C Real Time Clock Module (to control when the arduino should wake up and measure and transmit data)
+- Used libraries:
+    - DS3232RTC
+    - avr/sleep
+    - SPI
+    - nRF24L01
+    - RF24
+    - DHT
+
+Receiver:
+- Hardware components:
+    - Arduino board
+    - NRF24l01+ chip (to receive data)
+- Used libraries
+    - SPI
+    - nRF24L01
+    - RF24
+
+Intermediate transceiver
+- Not in development yet but should work kind of like a mix between the transmitter and the receiver
+
+
+//======================================================================//
+TODO
+Must have:
+Intermediate transceiver: receives data from up to 6 transmitters and sends the data to another transceiver or the receiver. Necessary because the receiver can't receive data from more than 6 transmitters or transceivers.
+Receiver needs functionality to receive data from more than 1 transmitter, need to be able to receive data from 6 receivers
+- Transmitters needs functionality to send their measured data at different times so no sent data is lost
+The format of the transmitted data should be improved to be more effective and to take up less space
+Data that has to be set individually for the transmitters, transceivers and receivers should appear in the top so they are easier to configure
+
+Should have:
+The code for setting the alarms in the transmitter should be able to set an arbitrary amount of alarms at different times. Right now there can only be 2 alarms but the beekeeper could change his mind
+
+Could have:
+The receiver could check the received temperature and humidity to find out if the values are too high or too low. The temperature should not fall below 24 degrees or exeed 35 degrees. The humidity should not fall below 40% or exceed 80%. The receiver could send an SMS to the beekepper to alert him when the values are too high or too low. This could also be checked by the transmitters and shown physically at the beehives.
---
+++ Transmitter/Transmitter.ino
@@ -0,0 +1,255 @@
+//Transmitter
+
+/////////////--- Place for varibles to change 
+// for testing, hours and minutes can be set here
+int testHour1 = 14;
+//int testHour2 = 14;
+int testMinute1 = 1;
+int testMinute2 = 2;
+
+//Sets the hours and minutes for the alarms
+//int hour1 = 12;
+//int hour2 = 16;
+//int minute1 = 0;
+//int minute2 = 0;
+/////////////---
+
+
+
+// RTC includes
+#include <DS3232RTC.h>
+#include <avr/sleep.h>
+#define interruptPin 2 
+
+
+//NRF includes
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
+//DHT includes
+#include "DHT.h"
+
+//NRF definitions
+#define CE_PIN  9
+#define CSN_PIN 10
+
+//DHT definitions
+#define DHTPIN 3 
+#define DHTTYPE DHT11 
+DHT dht(DHTPIN, DHTTYPE);
+
+//NRF constants
+const byte slaveAddress[5] = {'R','x','A','A','A'};
+RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
+char dataToSend[9]; //testDataToSend = {'0','1','+','0','7','5','0','7','3'};
+
+//has to be unique for each transmitter and between 1 and 6
+//should be marked on the transmitter physically with a label
+const int transmitterId = 1;
+
+//time_t variable used when sleeping and rearming alarms
+ time_t t;
+
+void setup() {
+//DHT setup
+    dht.begin();
+
+//RFC setup
+    pinMode(3,OUTPUT);
+    digitalWrite(3,HIGH);
+    Serial.begin(9600);
+    pinMode(7,OUTPUT); // NRF trnsistor pin
+
+// Initiates the Alarms, and refreshes them. 
+    pinMode(interruptPin,INPUT_PULLUP);
+    RTC.setAlarm(ALM1_MATCH_DATE, 0, 0, 0, 1);
+    RTC.setAlarm(ALM2_MATCH_DATE, 0, 0, 0, 1);
+    RTC.alarm(ALARM_1);
+    RTC.alarm(ALARM_2);
+    RTC.alarmInterrupt(ALARM_1, false);
+    RTC.alarmInterrupt(ALARM_2, false);
+    RTC.squareWave(SQWAVE_NONE);
+
+// Arms alarm 1 
+    RTC.setAlarm(ALM1_MATCH_HOURS,0,testMinute1,testHour1,0);
+    RTC.alarm(ALARM_1);
+    RTC.squareWave(SQWAVE_NONE);
+    RTC.alarmInterrupt(ALARM_1, true);
+
+// Arms Alarm 2
+    RTC.setAlarm(ALM2_MATCH_HOURS,0,testMinute2,testHour1,0);
+    RTC.alarm(ALARM_2);
+    RTC.squareWave(SQWAVE_NONE);
+    RTC.alarmInterrupt(ALARM_2, true);
+    
+   
+}
+
+void loop() {
+    initiateNRF();
+    
+    measureAndSaveToArray();
+    
+    send();
+    
+    sleepAndWakeUp();
+
+    rearmAlarms();
+}
+
+//=====================================================================================//
+//Custom methods
+//=====================================================================================//
+
+
+//Initiates the NRF chip, has to be done every time before sending the data
+//Also starts the interruptpin (pin 7)
+void initiateNRF(){
+    Serial.println("SimpleTx Starting");
+    digitalWrite(7,HIGH);
+    delay(10);
+    
+    radio.begin();
+    radio.flush_tx();
+    radio.setAutoAck(true);
+    radio.setDataRate( RF24_250KBPS );
+    radio.setRetries(3,5); // delay, count
+    radio.stopListening();
+    radio.openWritingPipe(slaveAddress);
+    delay(10);
+}
+
+
+//Measures humidity and temperature and converts them into the format expected from the receiver:
+//The format that the receiver can receive is: ['x','x','y','y','y','y','z','z','z'] where x=id, y=temperature, z=humidity
+void measureAndSaveToArray(){
+  int hum = dht.readHumidity();
+  float temp = dht.readTemperature();
+
+  //converting id, humidity and temperature to strings to format them into array
+  String tId = String(transmitterId);
+  String tHum = String(hum);
+  String tTemp = String(temp);
+  
+  //Formatting transmitter id into array
+  if(transmitterId < 10){
+    dataToSend[0] = '0';
+    dataToSend[1] = tId[0];
+  }
+  else{
+    dataToSend[0] = tId[0];
+    dataToSend[1] = tId[1];
+  }
+
+  //Formatting temperature into array
+  if(temp >= 0){
+    dataToSend[2] = '+';
+  }
+ 
+  if(temp < 100 && temp >= 10){
+    dataToSend[3] = tTemp[0];
+    dataToSend[4] = tTemp[1];
+    dataToSend[5] = tTemp[3];
+  }
+  else if(temp < 10 && temp > 0){
+    dataToSend[3] = '0';
+    dataToSend[4] = tTemp[0];
+    dataToSend[5] = tTemp[2];
+  }
+  else if(temp < 0 && temp > -10){
+    dataToSend[2] = tTemp[0];
+    dataToSend[3] = '0';
+    dataToSend[4] = tTemp[1];
+    dataToSend[5] = tTemp[3];
+  }
+  else if(temp < -10){
+    dataToSend[2] = tTemp[0];
+    dataToSend[3] = tTemp[1];
+    dataToSend[4] = tTemp[2];
+    dataToSend[5] = tTemp[5];
+  }
+
+  //formatting humidity into array
+  if(hum < 100){
+    dataToSend[6] = '0';
+  }
+  else{
+     dataToSend[6] = '1';
+     dataToSend[7] = '0';
+     dataToSend[8] = '0';
+  }
+  
+  if(hum < 10){
+    dataToSend[7] = '0';
+    dataToSend[8] = tHum[0];
+  }
+  else{
+    dataToSend[7] = tHum[0];
+    dataToSend[8] = tHum[1];
+  }
+}
+
+//tries to send the dataToSend
+void send() {
+    bool succeded = radio.write( &dataToSend, sizeof(dataToSend) );
+    Serial.print("Data Sent, sent the array: ");
+    Serial.print(dataToSend);
+    
+    if (succeded) {
+        Serial.println("  Acknowledgement received, data sent succesfully");
+    }
+    else {
+        Serial.println("  Tx failed");
+    }
+}
+
+//Sets the interrupt, puts the transmitter to sleep and handles the waking up 
+void sleepAndWakeUp(){
+  sleep_enable();
+  
+  //Attaches the interrupt
+  attachInterrupt(0, interrupt, LOW); 
+
+  //Puts transmitter to sleep
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); 
+  t=RTC.get();
+  Serial.println("Sover nu, klokken er: "+String(hour(t))+":"+String(minute(t))+":"+String(second(t)));
+  digitalWrite(7,LOW); // Turns off the transistor to the NRF chip
+  delay(1000); //gives time for everything to turn off before the transmitter is put to sleep
+  sleep_cpu();
+
+  //Prints the time when the sleep process is interrupted and the transmitter wakes up
+  t=RTC.get();
+  Serial.println("Vågen nu, klokken er: "+String(hour(t))+":"+String(minute(t))+":"+String(second(t)));
+ 
+}
+
+//Rearms the alarms after waking up
+void rearmAlarms(){
+  //Rearms alarm1
+  if (testMinute1 == minute(t)) {
+      RTC.alarm(ALARM_1);
+      Serial.println("Slukker alarm 1");
+      delay(10);
+      RTC.setAlarm(ALM1_MATCH_HOURS,0,testMinute1,testHour1,0);
+      delay(10); 
+  }
+  //Rearms alarm2
+  if (testMinute2 == minute(t)) {
+    RTC.alarm(ALARM_2);
+    Serial.println("Slukker alarm 2");
+    delay(10);
+    RTC.setAlarm(ALM2_MATCH_HOURS,0,testMinute2,testHour1,0);
+    delay(10);
+  }
+}
+
+//used to interrupt the sleeping process 
+void interrupt(){
+  Serial.println("Interrrupt Fired");
+  sleep_disable();//Disable sleep mode
+  detachInterrupt(0); //Removes the interrupt from pin 2;
+  delay(1000);
+}
